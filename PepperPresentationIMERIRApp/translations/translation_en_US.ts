0b816de493f923838fc518632bfd0e882f1b1e74
Eric Ghoubiguian <ericghoubiguian@live.fr>
2021-09-14 08:55:25 +0200

    My namle is... Choregraphe

--- PepperPresentationIMERIRApp/translations/translation_en_US.ts
+++ PepperPresentationIMERIRApp/translations/translation_en_US.ts
@@ -8,9 +8,29 @@
             <comment>Text</comment>
             <translation type="vanished">Hello</translation>
         </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Bonjour</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Message de bienvenue au prospect</name>
         <message>
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Bonjour à toi, humain ! Je m'appelle Pepper. Je suis un robot humanoïde, un système complexe associant la mécanique, l'électronique, et l'informatique. L'informatique ? Parlons-en ! Ma grande force, c'est l'intelligence qu'on m'a développée, et qui me permet de te parler et de t'exposer des choses. Mais qui me permet également de recueillir ton avis, de te répondre à une question, et de réagir selon les différentes émotions que tu peux éprouver. Mon intelligence, tu l'as compris, a été programmé, par des programmeurs. Et toi aussi, tu peux apprendre le développement informatique, pour que développer un robot comme moi, n'ait plus de secrets pour toi. Tu l'as compris, je suis ici en tant qu'ambassadeur de l'IMÉRIR. Acronyme d'institut méditerranéen d'étude et de recherche en informatique et robotique, cette école te permet, justement, de te former aux métiers du développement informatique, de la robotique, des objets connectés et de l'intelligence artificielle, pour que toi aussi, tu sois capable de me programmer, voire, de développer des choses plus balèzes encore. Quelles choses passionnantes !</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Qui sommes nous ?</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>L’IMÉRIR est une école supérieure de la C C I, pour 'Chambre de commerce et d'industrie', des Pyrénées Orientales, fondée en 1981, et basée à Perpignan, plus exactement sur 'Purple campus'. L’IMÉRIR prépare aux métiers de la robotique, de l’informatique, des objets connectés, et de l'intelligence artificielle. Le management et la gestion de projets y sont également abordés. Notre école dispose aussi d'un fablab, un lieu où sont disponibles des imprimantes 3D, une découpeuse laser, des robots industriels, comme mes amis robots de chez Kouka, Fanuc, et Staubli, des fers à souder, du matériel électronique et tous ce qui est nécessaire, pour le développement de projets maikeur, et qui sont utiles pour réaliser des projets d'école, des projets personnels, voire des projets d'ordre professionnel. L’IMÉRIR cultive aussi des partenariats, comme avec le groupe école Pratique à Marseille, le centre de formation de la C C I Marseille-Provence.</source>
             <comment>Text</comment>
             <translation type="unfinished"></translation>
         </message>
