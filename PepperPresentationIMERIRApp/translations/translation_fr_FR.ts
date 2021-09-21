9f6ba22e79a3a6deeaa2d61ea40f87822a01fbd9
Eric Ghoubiguian <ericghoubiguian@live.fr>
2021-09-21 11:37:48 +0200

    Reborn 3

--- PepperPresentationIMERIRApp/translations/translation_fr_FR.ts
+++ PepperPresentationIMERIRApp/translations/translation_fr_FR.ts
@@ -14,10 +14,9 @@
             <translation type="obsolete">Bonjour à toi, humain ! Je m'appelle Pepper. Je suis un robot humanoïde, un système complexe associant la mécanique, l'électronique, et l'informatique. L'informatique ? Parlons-en ! Ma grande force, c'est l'intelligence qu'on m'a développée, et qui me permet de te parler et de t'exposer des choses. Mais qui me permet également de recueillir ton avis, de te répondre à une question, et de réagir selon les différentes émotions que tu peux éprouver. Mon intelligence, tu l'as compris, a été programmé, par des programmeurs. Et toi aussi, tu peux apprendre le développement informatique, pour que développer un robot comme moi, n'ait plus de secrets pour toi. Tu l'as compris, je suis ici en tant qu'ambassadeur de l'IMÉRIR. Acronyme d'institut méditerranéen d'étude et de recherche en informatique et robotique, cette école te permet, justement, de te former aux métiers du développement informatique, de la robotique, des objets connectés et de l'intelligence artificielle, pour que toi aussi, tu sois capable de me programmer, voire, de développer des choses plus balèzes encore. Quelles choses passionnantes !</translation>
         </message>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Moi aussi</source>
             <comment>Text</comment>
-            <translation type="unfinished">Moi aussi</translation>
+            <translation type="obsolete">Moi aussi</translation>
         </message>
     </context>
     <context>
@@ -28,10 +27,18 @@
             <translation type="vanished">Bonjour</translation>
         </message>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Oh !!!!</source>
             <comment>Text</comment>
-            <translation type="unfinished">Oh !!!!</translation>
+            <translation type="obsolete">Oh !!!!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Le Fablab</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>L’IMÉRIR dispose d’un fablab, un espace numérique dédié à des fins d’innovation, de prototypage, ainsi que de production à petite échelle, et qui te permet d'accéder à des imprimantes 3D, des machines-outils habituellement réservées à des professionnels, du matériel électronique, du matériel pour la réalité virtuelle, des robots humanoïdes comme moi, des cobotes, et des robots industriels. Dans cet espace, les ressources matérielles, logicielles, et humaines sont mutualisées pour prototyper, expérimenter, travailler sur les projets donnés par l'école, ainsi que t'accompagner dans ton projet de prototypage, et de création d'entreprise. Plus ton projet est ambitieux et innovant, plus il est intéressant, et plus le Fablab se donnera pour défi de t'y accompagner, dans tous ses aspects, jusqu'à sa concrétisation complète. De grands moyens pour de grandes ambitions !</source>
+            <comment>Text</comment>
+            <translation type="unfinished">L’IMÉRIR dispose d’un fablab, un espace numérique dédié à des fins d’innovation, de prototypage, ainsi que de production à petite échelle, et qui te permet d'accéder à des imprimantes 3D, des machines-outils habituellement réservées à des professionnels, du matériel électronique, du matériel pour la réalité virtuelle, des robots humanoïdes comme moi, des cobotes, et des robots industriels. Dans cet espace, les ressources matérielles, logicielles, et humaines sont mutualisées pour prototyper, expérimenter, travailler sur les projets donnés par l'école, ainsi que t'accompagner dans ton projet de prototypage, et de création d'entreprise. Plus ton projet est ambitieux et innovant, plus il est intéressant, et plus le Fablab se donnera pour défi de t'y accompagner, dans tous ses aspects, jusqu'à sa concrétisation complète. De grands moyens pour de grandes ambitions !</translation>
         </message>
     </context>
     <context>
@@ -64,6 +71,15 @@
             <source>Merci de m'avoir laissé te présenter l'IMÉRIR. J'espère que ça a attisé ta curiosité dans les domaines de l'informatique, de la robotique, des objets connectés et de l'intelligence artificielle. Si tu es intéressé, tu peux demander à mes collègues humains des précisions supplémentaires, les plaquettes de présentation détaillée de chacune des formations dispensées par l'école, voire aussi un dossier d'inscription pour devenir étudiant de notre école et toutes informations supplémentaires. Tu peux aussi à tout moment demander les renseignements et les informations que tu veux, soit en te rendant sur le site officiel de l'IMÉRIR, soit en téléphonant au 04 68 56 80 18. Je répète, le 04 68 56 80 18. Tu peux aussi venir nous voir directement dans nos locaux. Ils sont situés sur le Purple campus de Perpignan, au 1, Avenue Paul Pascot, 66000, Perpignan. Je répète, au 1, Avenue Paul Pascot, 66000, Perpignan. Au revoir, j'espère te revoir très vite.</source>
             <comment>Text</comment>
             <translation type="unfinished">Merci de m'avoir laissé te présenter l'IMÉRIR. J'espère que ça a attisé ta curiosité dans les domaines de l'informatique, de la robotique, des objets connectés et de l'intelligence artificielle. Si tu es intéressé, tu peux demander à mes collègues humains des précisions supplémentaires, les plaquettes de présentation détaillée de chacune des formations dispensées par l'école, voire aussi un dossier d'inscription pour devenir étudiant de notre école et toutes informations supplémentaires. Tu peux aussi à tout moment demander les renseignements et les informations que tu veux, soit en te rendant sur le site officiel de l'IMÉRIR, soit en téléphonant au 04 68 56 80 18. Je répète, le 04 68 56 80 18. Tu peux aussi venir nous voir directement dans nos locaux. Ils sont situés sur le Purple campus de Perpignan, au 1, Avenue Paul Pascot, 66000, Perpignan. Je répète, au 1, Avenue Paul Pascot, 66000, Perpignan. Au revoir, j'espère te revoir très vite.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Qui sommes nous ?</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>L’IMÉRIR est une école supérieure de la C C I, pour &quot;Chambre de commerce et d'industrie&quot;, des Pyrénées Orientales, fondée en 1981, et basée à Perpignan, plus exactement sur 'Purple campus'. L’IMÉRIR prépare aux métiers de la robotique, de l’informatique, des objets connectés, et de l'intelligence artificielle. Le management et la gestion de projets y sont également abordés. Notre école dispose aussi d'un fablab, un lieu où sont disponibles des imprimantes 3D, une découpeuse laser, des robots industriels, comme mes amis robots de chez Kouka, Fanuc, et Staubli, des fers à souder, du matériel électronique et tous ce qui est nécessaire, pour le développement de projets maikeur, et qui sont utiles pour réaliser des projets d'école, des projets personnels, voire des projets d'ordre professionnel. L’IMÉRIR cultive aussi des partenariats, comme avec le groupe école Pratique à Marseille, le centre de formation de la C C I Marseille-Provence.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">L’IMÉRIR est une école supérieure de la C C I, pour &quot;Chambre de commerce et d'industrie&quot;, des Pyrénées Orientales, fondée en 1981, et basée à Perpignan, plus exactement sur 'Purple campus'. L’IMÉRIR prépare aux métiers de la robotique, de l’informatique, des objets connectés, et de l'intelligence artificielle. Le management et la gestion de projets y sont également abordés. Notre école dispose aussi d'un fablab, un lieu où sont disponibles des imprimantes 3D, une découpeuse laser, des robots industriels, comme mes amis robots de chez Kouka, Fanuc, et Staubli, des fers à souder, du matériel électronique et tous ce qui est nécessaire, pour le développement de projets maikeur, et qui sont utiles pour réaliser des projets d'école, des projets personnels, voire des projets d'ordre professionnel. L’IMÉRIR cultive aussi des partenariats, comme avec le groupe école Pratique à Marseille, le centre de formation de la C C I Marseille-Provence.</translation>
         </message>
     </context>
 </TS>
